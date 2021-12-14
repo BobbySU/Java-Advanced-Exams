14d0568a183631c3945de57fb29d48efa77a7d91
BobbySU <radio_20bg@yahoo.com>
2021-12-15 00:09:21 +0200

    Add 3. Hotel Update

--- Regular Exam 23.10/src/hotel/Hotel.java
+++ Regular Exam 23.10/src/hotel/Hotel.java
@@ -2,29 +2,28 @@ package hotel;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 public class Hotel {
     private String name;
     private int capacity;
-    private List<Person> personList;
+    private List<Person> roster;
 
     public Hotel(String name, int capacity) {
         this.name = name;
         this.capacity = capacity;
-        this.personList = new ArrayList<>();
+        this.roster = new ArrayList<>();
     }
 
     public void add(Person person) {
-        if (this.personList.size() < this.capacity) {
-            this.personList.add(person);
+        if (this.roster.size() < this.capacity) {
+            this.roster.add(person);
         }
     }
 
     public boolean remove(String name) {
-        for (int i = 0; i < this.personList.size(); i++) {
-            if (this.personList.get(i).getName().equals(name)) {
-                this.personList.remove(i);
+        for (int i = 0; i < this.roster.size(); i++) {
+            if (this.roster.get(i).getName().equals(name)) {
+                this.roster.remove(i);
                 return true;
             }
         }
@@ -32,23 +31,23 @@ public class Hotel {
     }
 
     public Person getPerson(String name, String hometown) {
-        for (int i = 0; i < this.personList.size(); i++) {
-            if (this.personList.get(i).getName().equals(name)
-                    && this.personList.get(i).getHometown().equals(hometown)) {
-                return this.personList.get(i);
+        for (int i = 0; i < this.roster.size(); i++) {
+            if (this.roster.get(i).getName().equals(name)
+                    && this.roster.get(i).getHometown().equals(hometown)) {
+                return this.roster.get(i);
             }
         }
         return null;
     }
 
     public int getCount() {
-        return this.personList.size();
+        return this.roster.size();
     }
 
     public String getStatistics() {
         StringBuilder sb = new StringBuilder();
         sb.append(String.format("The people in the hotel %s are:%n", this.name));
-        for (Person e : personList) {
+        for (Person e : roster) {
             sb.append(e.toString()).append(System.lineSeparator());
         }
         return sb.toString().trim();
